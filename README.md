e61609e02364336ea345996532b03dc22d38a37c
khuonghoanghuy <khuonghoanghuy@gmail.com>
2023-10-16 20:35:55 +0700

    upload this anyways

---
+++ README.md
@@ -0,0 +1,4 @@
+# PowPer - A Normally Game ;-;
+Yea, this game is just like shooting scroll background and `like chicken invaders`, idk
+
+in-develop wait pls
---
+++ source/MenuState.hx
@@ -0,0 +1,22 @@
+package;
+
+import flixel.FlxState;
+import flixel.ui.FlxButton;
+
+class MenuState extends FlxState
+{
+	// list of button
+	var startButton:FlxButton;
+	var settingButton:FlxButton;
+	var quitButton:FlxButton;
+
+	override function create()
+	{
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
